630be606e774e4e030b655ac534f06ed9e8abe43
GleamTech <marketing@gleamtech.com>
2026-08-18 14:32:41 -0700

    Version 10.4.0
    ## Version 10.4.0 - August 18, 2026
    
      - **Improved:** Vastly improved startup performance:
    
        - Optimized internal AssemblyResolver, it now avoids pre-extracting native DLLs for modern .NET and subscribes
          to `AssemblyLoadContext.Default.ResolvingUnmanagedDll` event to extract a native DLL only when it's requested.
          This improves the cold-startup time of the host app for modern .NET.
          .NET Framework still needs to pre-extract native DLLs as it does not have that event but other optimizations
          in AssemblyResolver still improves that runtime.
          AssemblyResolver.log now includes more details about resolved assemblies and has more readable/neat text structure.
    
        - Minimized reflection type scanning in GleamTech assemblies to prevent early loading of dependency assemblies.
    
        - Considerably reduced size of GleamTech DLLs to decrease memory footprint and improve loading time.
          (further reducing will be done in next versions by minimizing dependencies)
    
      - **Improved:** Upgraded `AspNetCoreCS` example projects of all products from `net6.0` to `net8.0` (Visual Studio 2022+ (17.8+)).
        Updated `Getting Started` docs and made code samples up-to date.
    
      - **Changed:** Removed implicit operators for casting to/from other libraries (e.g. ImageSharp, SkiaSharp),
        from `Image`, `Color`, `Font` and `Rectangle` classes in `GleamTech.Drawing` namespace,
        and made them extension methods in another class to prevent reflection method scanning to unnecessarily probe assemblies.
        Also shortened the names of the conversion methods:
        - ToGleamTechDrawingImage() -> ToGleamTechImage()
        - ToSystemDrawingBitmap() -> ToSystemBitmap()
        - ToSystemDrawingImage -> ToSystemImage()
        - ToSkiaSharpBitmap -> ToSkiaBitmap()
        - ToSkiaSharpBitmap -> ToSkiaBitmap()

--- Examples/AspNetCoreCS/Program.cs
+++ Examples/AspNetCoreCS/Program.cs
@@ -1,20 +1,54 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using GleamTech;
+using GleamTech.AspNet;
+using GleamTech.AspNet.Core;
+using GleamTech.FileUltimate;
 
-namespace GleamTech.FileUltimateExamples.AspNetCoreCS
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+
+//----------------------
+//Add GleamTech to the ASP.NET Core services container.
+builder.Services.AddGleamTech();
+//----------------------
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    app.UseExceptionHandler("/Home/Error");
 }
+
+
+//----------------------
+//Register GleamTech to the ASP.NET Core HTTP request pipeline.
+app.UseGleamTech(() =>
+{
+    //The below custom config file loading is only for our demo publishing purpose:
+
+    var gleamTechConfig = Hosting.ResolvePhysicalPath("~/App_Data/GleamTech.config");
+    if (File.Exists(gleamTechConfig))
+        GleamTechConfiguration.Current.Load(gleamTechConfig);
+
+    var fileUltimateConfig = Hosting.ResolvePhysicalPath("~/App_Data/FileUltimate.config");
+    if (File.Exists(fileUltimateConfig))
+        FileUltimateConfiguration.Current.Load(fileUltimateConfig);
+});
+//----------------------
+
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
